3abea51a2c99973200b0bc5190bc38dd89a71b0b
Ranit Debnath Akash <ranitid12@gmail.com>
2022-06-17 21:41:24 +0600

    Add Next+TS boilerplate!

---
+++ next.config.js
@@ -0,0 +1,12 @@
+
+    /** @type {import('next').NextConfig} */
+    const nextConfig = {
+      reactStrictMode: true,
+      images: {
+        domains: ["www.recruit.so", "www.reframe.so", "reframe.so"],
+      },
+    };
+  
+    module.exports = nextConfig;
+  
+    
